fb8c8e4919c4ca749441efb7903bd48d5f99d98f
oneillsimon <oneill.simon92@gmail.com>
2014-07-12 14:02:04 +0100

    Defined Vector4

---
+++ GameEngine/Engine/Core/Vector4.cpp
@@ -0,0 +1,58 @@
+#include "3DMath.h"
+
+Vector4::Vector4()
+{
+	Vector4(0, 0, 0, 0);
+}
+
+Vector4::Vector4(float x, float y, float z, float w)
+{
+	this->x = x;
+	this->y = y;
+	this->z = z;
+	this->w = w;
+}
+
+Vector4::~Vector4()
+{
+}
+
+float Vector4::getX() const
+{
+	return x;
+}
+
+float Vector4::getY() const
+{
+	return y;
+}
+
+float Vector4::getZ() const
+{
+	return z;
+}
+
+float Vector4::getW() const
+{
+	return w;
+}
+
+void Vector4::setX(float x)
+{
+	this->x = x;
+}
+
+void Vector4::setY(float y)
+{
+	this->y = y;
+}
+
+void Vector4::setZ(float z)
+{
+	this->z = z;
+}
+
+void Vector4::setW(float w)
+{
+	this->w = w;
+}
