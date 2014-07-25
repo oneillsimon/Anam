c0c40357f97e4cce74bb78d45a0c3441f9ed1900
oneillsimon <oneill.simon92@gmail.com>
2014-07-25 13:51:13 +0100

    Added constructors for normal mapping

--- GameEngine/Engine/Rendering/Vertex.cpp
+++ GameEngine/Engine/Rendering/Vertex.cpp
@@ -2,37 +2,56 @@
 
 Vertex::Vertex(Vector3 position, Vector2 texCoord, Vector3 normal)
 {
-	this->position = position;
-	this->texCoord = texCoord;
-	this->normal = normal;
+	m_position = position;
+	m_texCoord = texCoord;
+	m_normal = normal;
+	m_tangent = Vector3::ZERO;
+}
+
+Vertex::Vertex(Vector3 position, Vector2 texCoord, Vector3 normal, Vector3 tangent)
+{
+	m_position = position;
+	m_texCoord = texCoord;
+	m_normal = normal;
+	m_tangent = tangent;
 }
 
 Vector3 Vertex::getPosition()
 {
-	return position;
+	return m_position;
 }
 
 Vector2 Vertex::getTexCoord()
 {
-	return texCoord;
+	return m_texCoord;
 }
 
 Vector3 Vertex::getNormal()
 {
-	return normal;
+	return m_normal;
+}
+
+Vector3 Vertex::getTangent()
+{
+	return m_tangent;
 }
 
 void Vertex::setPosition(Vector3 position)
 {
-	this->position = position;
+	m_position = position;
 }
 
 void Vertex::setTexCoord(Vector2 texCoord)
 {
-	this->texCoord = texCoord;
+	m_texCoord = texCoord;
 }
 
 void Vertex::setNormal(Vector3 normal)
 {
-	this->normal = normal;
+	m_normal = normal;
+}
+
+void Vertex::setTangent(Vector3 tangent)
+{
+	m_tangent = tangent;
 }
--- GameEngine/Engine/Rendering/Vertex.h
+++ GameEngine/Engine/Rendering/Vertex.h
@@ -6,20 +6,24 @@
 class Vertex
 {
 private:
-	Vector3 position;
-	Vector2 texCoord;
-	Vector3 normal;
+	Vector3 m_position;
+	Vector2 m_texCoord;
+	Vector3 m_normal;
+	Vector3 m_tangent;
 
 public:
 	Vertex(Vector3 position, Vector2 texCoord, Vector3 normal);
+	Vertex(Vector3 position, Vector2 texCoord, Vector3 normal, Vector3 tangent);
 
 	Vector3 getPosition();
 	Vector2 getTexCoord();
 	Vector3 getNormal();
+	Vector3 getTangent();
 
 	void setPosition(Vector3 position);
 	void setTexCoord(Vector2 texCoord);
 	void setNormal(Vector3 normal);
+	void setTangent(Vector3 tangent);
 };
 
 #endif
