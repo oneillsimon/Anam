a78ec22e3188f9a100119729948ffb5d2158e5fb
oneillsimon <oneill.simon92@gmail.com>
2014-08-24 14:11:09 +0100

    Added Physic Engine

---
+++ GameEngine/Engine/Physics/PhysicsEngine.cpp
@@ -0,0 +1,39 @@
+#include "BoundingSphere.h"
+#include "PhysicsEngine.h"
+
+PhysicsEngine::PhysicsEngine()
+{
+}
+
+void PhysicsEngine::addObject(const PhysicsObject& object)
+{
+	m_objects.push_back(object);
+}
+
+void PhysicsEngine::simulate(float delta)
+{
+	for(unsigned int i = 0; i < m_objects.size(); i++)
+	{
+		m_objects[i].integrate(delta);
+	}
+}
+
+void PhysicsEngine::handleCollisions()
+{
+	for(unsigned int i = 0; i < m_objects.size(); i++)
+	{
+		for(unsigned int j = i + 1; j < m_objects.size(); j++)
+		{
+			IntersectionData intersectData = m_objects[i].getCollider().intersect(m_objects[j].getCollider());
+
+			if(intersectData.getDoesIntersect())
+			{
+				Vector3 direction = intersectData.getDirection().normalised();
+				Vector3 otherDirection = Vector3(direction.reflect(m_objects[i].getVelocity().normalised()));
+
+				m_objects[i].setVelocity(Vector3(m_objects[i].getVelocity().reflect(otherDirection)));
+				m_objects[j].setVelocity(Vector3(m_objects[j].getVelocity().reflect(direction)));
+			}
+		}
+	}
+}
---
+++ GameEngine/Engine/Physics/PhysicsEngine.h
@@ -0,0 +1,31 @@
+#ifndef PHYSICSENGINE_H
+#define PHYSICSENGINE_H
+
+#include <vector>
+#include "PhysicsObject.h"
+
+class PhysicsEngine
+{
+private:
+	std::vector<PhysicsObject> m_objects;
+
+public:
+	PhysicsEngine();
+
+	void addObject(const PhysicsObject& object);
+	void simulate(float delta);
+	void handleCollisions();
+
+	//TODO: temp
+	const PhysicsObject& getObject(unsigned int index) const
+	{
+		return m_objects[index];
+	}
+
+	unsigned int getNumObjects() const
+	{
+		return (unsigned int)m_objects.size();
+	}
+};
+
+#endif
