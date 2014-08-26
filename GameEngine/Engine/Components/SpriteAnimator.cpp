4d76ce337c0c3b655d0a1979a3ac5bc3c0a87aed
oneillsimon <oneill.simon92@gmail.com>
2014-08-26 13:26:21 +0100

    Added class to animate through the spritesheet

---
+++ GameEngine/Engine/Components/SpriteAnimator.cpp
@@ -0,0 +1,19 @@
+#include "SpriteAnimator.h"
+
+SpriteAnimator::SpriteAnimator(const SpriteSheet& spriteSheet, float every, bool upCondition) :
+	m_spriteSheet(spriteSheet),
+	m_speed(every)
+{
+	m_timer = 0;
+}
+
+void SpriteAnimator::update(float delta)
+{
+	m_timer += delta * 1000.0f;
+
+	if(true && m_timer > m_speed)
+	{
+		m_spriteSheet.cycleUp(delta);
+		m_timer = 0;
+	}
+}
---
+++ GameEngine/Engine/Components/SpriteAnimator.h
@@ -0,0 +1,20 @@
+#ifndef SPRITEANIMATOR_H
+#define SPRITEANIMATOR_H
+
+#include "../Rendering/SpriteSheet.h"
+#include "GameComponent.h"
+
+class SpriteAnimator : public GameComponent
+{
+private:
+	SpriteSheet m_spriteSheet;
+	float m_speed;
+	float m_timer;
+
+public:
+	SpriteAnimator(const SpriteSheet& spriteSheet, float every, bool upCondition);
+
+	virtual void update(float delta);
+};
+
+#endif
